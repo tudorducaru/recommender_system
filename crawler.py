046bc72a4f6c23d6512f3a7b5ee76ddae0a33fb4
tudorducaru <tducaru@gmail.com>
2021-10-31 18:35:21 +0200

    basic crawling functionality

--- crawler.py
+++ crawler.py
@@ -4,30 +4,52 @@ from bs4 import BeautifulSoup
 # list of all feeds
 feeds = []
 
+unvisitedListUrls = []
+visitedListUrls = []
+
 # retrieves all rss feed links from the given url
 # and adds them to the global variable
-def getFeedsFromUrl(url):
+def scrapeUrl(url):
 
     # retrieve the webpage content
     # include user-agent to ensure the response is not 403 Forbidden
     headers = {'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Mobile Safari/537.36'}
-    webpage_response = requests.get('https://blog.feedspot.com/montreal_rss_feeds/', headers=headers)
+    webpage_response = requests.get(url, headers=headers)
 
     # get the rss feeds on that webpage
     soup = BeautifulSoup(webpage_response.content, 'html.parser')
     for tag in soup.select('.trow .fa-rss + a'):
 
-        # check that the feed exists
         href = tag.attrs['href']
-        if href != '':
+        if href != '' and href not in feeds:
             feeds.append(tag.attrs['href'])
 
     # get the links to the feed lists on the page
-    lists = []
     for tag in soup.select('.et_pb_extra_column_sidebar a'):
 
         # href only contains the path, construct complete url
         listUrl = 'https://blog.feedspot.com' + tag.attrs['href']
-        lists.append(listUrl)
+        
+        # if the list has not been visited before,
+        # add it to the unvisited lists
+        if listUrl not in visitedListUrls:
+            unvisitedListUrls.append(listUrl)
 
-getFeedsFromUrl('https://blog.feedspot.com/montreal_rss_feeds/')
+# provide a starting URL to begin crawling
+def crawl(startingUrl):
+
+    unvisitedListUrls.append(startingUrl)
+
+    # scrape webpages until there are no more lists to be scraped
+    while unvisitedListUrls:
+
+        listUrl = unvisitedListUrls.pop(0)
+        print('Scraping list: ', listUrl)
+        scrapeUrl(listUrl)
+        print('Number of feeds: ', len(feeds))
+        print('\n')
+
+        # add the list URL to the visited URLs
+        visitedListUrls.append(listUrl)
+
+crawl('https://blog.feedspot.com/uk_rss_feeds/')

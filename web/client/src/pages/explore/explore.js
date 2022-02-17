18b00ea2f173975c11d0c576fd7a2611ddc2e6fb
tudorducaru <tducaru@gmail.com>
2022-02-17 09:56:26 +0000

    get feeds from the server

--- web/client/src/pages/explore/explore.js
+++ web/client/src/pages/explore/explore.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState }  from 'react';
 import './explore.css';
+import DataService from '../../services/dataService';
 
 const Explore = () => {
+
+    // Keep the feeds in local state
+    const [feeds, setFeeds] = useState([]);
+
+    // Load feeds from the database when the page loads
+    useEffect(() => {
+
+        DataService.getFeeds(0)
+            .then((data) => setFeeds(data))
+            .catch(errorMessage => console.log(errorMessage));
+
+    }, []);
+
+
     return (
-        <div>Explore</div>
+        <div>
+            <h1>Explore</h1>
+            { feeds.map((feed) => <p key={feed.id}>{feed.id}</p>) }
+        </div>
     )
 }
 
--- web/client/src/services/dataService.js
+++ web/client/src/services/dataService.js
@@ -3,8 +3,6 @@ import axios from 'axios';
 const API_URL = 'http://localhost:5000';
 
 class DataService {
-    
-    
 
     // Get all words 
     async getWords() {
@@ -13,6 +11,24 @@ class DataService {
             const response = await axios.get(
                 `${API_URL}/getWords`
             );
+            return response.data;
+
+        } catch (err) {
+            return err.response.data;
+        }
+    }
+
+    // Get all feeds for the given page
+    async getFeeds(page) {
+        try {
+
+            const response = await axios.get(
+                `${API_URL}/getFeeds`,
+                {
+                    params: { page }
+                }
+            );
+
             return response.data;
 
         } catch (err) {

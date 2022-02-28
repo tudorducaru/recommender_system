014660591ab4e55df72ee548c6b6bce7493ec5e5
tudorducaru <tducaru@gmail.com>
2022-02-28 18:31:38 +0000

    add layout for extension

--- web_extension/main.js
+++ web_extension/main.js
@@ -26,18 +26,27 @@ $(document).ready(async () => {
     const authenticated = await verifyUser();
 
     if (authenticated) {
-        $('#test').text('Authenticated');
 
         // Ask the content script to search RSS feeds
         chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
             chrome.tabs.sendMessage(tabs[0].id, { searchRSS: true }, response => {
-                $('#test').text(response.feed_url);
 
-                // Set a listener on the add feed button
-                $('#add-feed-btn').on('click', async () => {
+                if (!response.feed_url) {
 
-                    // Add the feed to the database
-                    try {
+                    $('.no-feeds-container').removeClass('d-none');
+
+                } else {
+
+                    $('.authenticated-container').removeClass('d-none');
+
+                    // Display the feed on the popup
+                    $('#feed-url').text(response.feed_url);
+
+                    // Set a listener on the add feed button
+                    $('#add-feed-btn').on('click', async () => {
+
+                        // Add the feed to the database
+
 
                         // Get csrf cookie
                         chrome.cookies.get({
@@ -45,37 +54,38 @@ $(document).ready(async () => {
                             name: 'csrf_access_token'
                         }, async csrf_cookie => {
 
-                            await axios.post(
-                                'http://localhost:5000/addLikedFeed',
-                                {
-                                    feed_url: response.feed_url
-                                },
-                                {
-                                    withCredentials: true,
-                                    headers: {
-                                        'X-CSRF-TOKEN': csrf_cookie.value
-                                    }
-                                }
+                            try {
+                                await axios.post(
+                                    'http://localhost:5000/addLikedFeed',
+                                    {
+                                        feed_url: response.feed_url
+                                    },
+                                    {
+                                        withCredentials: true,
+                                        headers: {
+                                            'X-CSRF-TOKEN': csrf_cookie.value
+                                        }
+                                    });
 
-                            )
-
+                                $('.authenticated-container').addClass('d-none');
+                                $('.result').removeClass('d-none');
+                                $('.success').removeClass('d-none');
+                            } catch (e) {
+                                $('.authenticated-container').addClass('d-none');
+                                $('.result').removeClass('d-none');
+                                $('.failure').removeClass('d-none');
+                            }
                         });
 
-
-
-                    } catch (e) {
-                        console.log(e);
-                        $('#test').text(e);
-                    }
-
-                })
+                    });
+                }
 
             });
         });
 
 
     } else {
-        $('#test').text('Not authenticated');
+        $('.not-authenticated-container').removeClass('d-none');
     }
 
 });
